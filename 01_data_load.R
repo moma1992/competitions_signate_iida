0a7abf0ae991a6306b6ee844d6f90391a2ea588e
rstudio <mamoiro@gmail.com>
2019-07-01 15:45:16 +0000

    fix

--- 01_data_load.R
+++ 01_data_load.R
@@ -1,9 +1,9 @@
-# install library ----------------------------------------------
+# inport library ------------------------------------------------
 library(tidyverse)
+
 
 # data load ----------------------------------------------------
 df_train_genba  <-  read_tsv("./input/train_genba.tsv")
 df_test_genba  <-  read_tsv("./input/test_genba.tsv")
 df_train_goto  <-  read_tsv("./input/train_goto.tsv")
-df_test_goto  <-  read_tsv("./input/train_goto.tsv")
-
+df_test_goto  <-  read_tsv("./input/test_goto.tsv")
