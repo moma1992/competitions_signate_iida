875d232b99f6289b0f0fab249dfb183aba3d6e21
rstudio <mamoiro@gmail.com>
2019-07-01 10:04:03 +0000

    add r script for data load

---
+++ 01_data_load.R
@@ -0,0 +1,9 @@
+# install library ----------------------------------------------
+library(tidyverse)
+
+# data load ----------------------------------------------------
+df_train_genba  <-  read_tsv("./input/train_genba.tsv")
+df_test_genba  <-  read_tsv("./input/test_genba.tsv")
+df_train_goto  <-  read_tsv("./input/train_goto.tsv")
+df_test_goto  <-  read_tsv("./input/train_goto.tsv")
+
