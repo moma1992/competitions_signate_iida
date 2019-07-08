a94cd5de1259e129c759a95a1814f89c3060364d
rstudio <mamoiro@gmail.com>
2019-07-08 07:44:48 +0000

    add fe processing

--- src/02_feature_engineering.R
+++ src/02_feature_engineering.R
@@ -1,6 +1,9 @@
 # inport library ------------------------------------------------
 library(tidyverse)
+library(magrittr)
 library(catboost)
+
+source('./utils/FE_function.R')
 
 # join
 remove_col <- c("id", "pj_no", "keiyaku_pr")
@@ -17,11 +20,15 @@ character_col <- c("jukyo",
                    "eki_nm2",
                    "bastei_nm2")
 
+# processing for train ------------------------------------------
 train <- df_train_goto %>% 
   dplyr::left_join(df_train_genba, by = 'pj_no') 
 
-test <- df_test_goto %>% 
-  dplyr::left_join(df_test_genba, by = 'pj_no') 
+# feature:levelplan
+train %<>% convert_levelplan()
+
+# feature:geo_data
+train %<>% get_geo_data()
 
 x_train <- train %>% 
   dplyr::select(-one_of(remove_col)) %>% 
@@ -30,6 +37,16 @@ x_train <- train %>%
 y_train <- train %>%
   dplyr::select(keiyaku_pr) %>%
   as.matrix()
+
+# processing for test ------------------------------------------
+test <- df_test_goto %>% 
+  dplyr::left_join(df_test_genba, by = 'pj_no') 
+
+# feature:levelplan
+test %<>% convert_levelplan()
+
+# feature:geo_data
+test %<>% get_geo_data()
 
 x_test <- test %>% 
   dplyr::select(-one_of(remove_col)) %>% 
